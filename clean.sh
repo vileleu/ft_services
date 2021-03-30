464582f79569f7e78ff0a64d2a0e6bb3a6268109
vileleu <vico@LAPTOP-F70Q2UCV.localdomain>
2021-03-30 02:16:39 +0200

    clean_ok+

--- clean.sh
+++ clean.sh
@@ -8,23 +8,16 @@ declare -i i=0
 
 for name in ${!services[@]}
 do
-	printf "${BLUE}$i%% ${RESET}${BLUE}. "
+	printf "${BLUE}. . . $i%%${RESET}"
 	i+=100/${#services[@]}
 	kubectl delete -f srcs/deployments/${services[name]}.yaml > /dev/null
 	kubectl delete -f srcs/services/${services[name]}.yaml > /dev/null
-	printf ". "
-	sleep 0.1
-	printf ". "
-	sleep 0.1
-	printf "."
-	sleep 0.1
-	printf ""
-	printf "${BLUE} ${services[name]} service successfully delete\n${RESET}"
+	printf "\b\b\b   \n${BLUE}---${services[name]} service successfully delete\n${RESET}"
 done
 
 kubectl delete -f srcs/metallb.yaml > /dev/null
 
-printf "${BLUE}100%${RESET}"
+printf "${BLUE}. . . 100%%${RESET}"
 
 printf "\n${GREEN}----------Clean complete !----------\n${BLUE}"
 
